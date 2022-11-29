970a3fc1ce1875b88345b5ac950ba2d9df37ebf2
luciano ramos <jlsr18@gmail.com>
2022-11-28 21:45:30 -0300

    hoje

--- crud-pessoa/classe-pessoa.php
+++ crud-pessoa/classe-pessoa.php
@@ -1,32 +1,32 @@
 <?php
 
-      class Pessoa  
+      class Pessoa
       {
  
 
     private $pdo;
 
-          public function_construct ($dbname,$host,$user ,$senha)
-     {
-
-         try
-         {
-         $this->pdo = new PDO('mysql:dbname='.$bdname.';host='.$host,$user,$senha);
-          }
-       catch(PDOException $e)
-         {
-       echo"Erro com banco de dados:".$se-> getMessage();
-       exit();
-         }
-       catch(Exception $e)
+        public function __construct('$dbname, $host, $user,$senha,')
         {
-       echo"Erro generico:".$se-> getMessage();
-       exit();
 
+          try
+         {
+         $this->pdo = new PDO("mysql:dbname=".$bdname.";host=".$host,$user,$senha);
+          }
+          catch(PDOException $e)
+          {
+             echo"Erro com banco de dados:".$se-> getMessage();
+           exit();
+          }
+            catch(Exception $e)
+           {
+              echo"Erro generico:".$se-> getMessage();
+               exit();
+
+            }
         }
-       }
-         // funcao para buscar dados e colocar no lado direito da tela
-        Public function buscar()
+         // funcao para buscar dados e colocar no lado direito da tela  
+         Public function buscar()
       {
       $res = array();  
       $cmd = $this->pdo->query("SELECT * FROM pessoa ORDER BY nome");
